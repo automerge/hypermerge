1c292088045a0114161e8d727971032583ee1830
Jim Pick <jim@backblend.com>
2018-01-12 17:28:25 -0800

    Add some more comments
    It's probably better to make it longer with extra comments.

--- examples/usage-example/first-computer.js
+++ examples/usage-example/first-computer.js
@@ -13,22 +13,22 @@ hm.on('ready', () => {
   console.log('Waiting for connections...')
   sw.on('connection', peer => {
     console.log('connected to', sw.connections.length, 'peers')
+    // When the second computer connects, it will pass a key for the
+    // hypercore that it will write it's changes to as 'userData'
     const remotePeerKey = peer.remoteUserData.toString()
+    // Clone existing changes from second computer and incorporate into
+    // our Automerge document. Also listen for new changes.
     hm.connectPeer(remotePeerKey)
   })
 
-  // Write some data to our document using Automerge
+  // Write some data to our Automerge document
   hm.change(doc => {
     doc.todos = []
-    doc.todos.push({
-      id: 1,
-      title: 'Read the JSON CRDT paper',
-      done: false
-    })
+    doc.todos.push({id: 1, title: 'Read JSON CRDT paper', done: false})
   })
   console.log('Initial todos:\n', hm.get().todos)
 
-  // Watch for changes
+  // When our Automerge document is updated, display the todos
   hm.doc.registerHandler(doc => {
     console.log('Doc changed:\n', doc.todos)
     sw.close() // All done, close swarm and exit
