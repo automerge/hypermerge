ba5bdae84e62873b30c94ec9cd536e304e12e3e3
Peter van Hardenberg <pvh@pvh.ca>
2018-02-20 23:30:49 -0800

    remove prompt for nickname to make example smaller

--- examples/chat/chat.js
+++ examples/chat/chat.js
@@ -11,11 +11,6 @@ if (argv.help || argv._.length > 1) {
 }
 
 let nick = argv.nick
-if (!argv.nick) {
-  const prompt = require('prompt-sync')()
-  nick = prompt('Enter your nickname: ')
-}
-
 const channelHex = argv._[0]
 
 const model = new Model({channelHex, nick})
