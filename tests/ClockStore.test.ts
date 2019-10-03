c36d544961daadf9121948caa5dbca65f8f75045
Matt Tognetti <mjtognetti@gmail.com>
2019-10-03 16:54:51 -0700

    Add ClockStore tests for multiple repos

--- tests/ClockStore.test.ts
+++ tests/ClockStore.test.ts
@@ -73,4 +73,58 @@ test('ClockStore', (t) => {
     t.deepEqual(clocks, expectedClocks)
     db.close()
   })
+
+  t.test('get with multiple repos', (t) => {
+    t.plan(2)
+    const db = SqlDatabase.open('test.db', true)
+    const clockStore = new ClockStore(db)
+
+    const repoId = 'repoId' as RepoId
+    const docId = 'abc123' as DocId
+    const clock = { abc123: 1, def456: 0 }
+    clockStore.update(repoId, docId, clock)
+
+    const docId2 = 'ghi789' as DocId
+    const clock2 = { ghi789: 2, jkl012: 0 }
+    clockStore.update(repoId, docId2, clock2)
+
+    const repoId2 = 'repoId2' as RepoId
+    const clock3 = { abc123: 1, def456: 1 }
+    clockStore.update(repoId2, docId, clock3)
+
+    const res2 = clockStore.get(repoId2, docId)
+
+    const clocks = clockStore.getMultiple(repoId, [docId, docId2])
+    const expectedClocks = {
+      [docId]: clock,
+      [docId2]: clock2,
+    }
+    t.deepEqual(clocks, expectedClocks)
+    t.deepEqual(res2, clock3)
+    db.close()
+  })
+
+  t.test('update with multiple repos', (t) => {
+    t.plan(2)
+    const db = SqlDatabase.open('test.db', true)
+    const clockStore = new ClockStore(db)
+
+    const repoId = 'repoId' as RepoId
+    const docId = 'abc123' as DocId
+    const clock = { abc123: 1, def456: 0 }
+    clockStore.update(repoId, docId, clock)
+
+    const repoId2 = 'repoId2' as RepoId
+    const clock2 = { abc123: 1, def456: 1 }
+    clockStore.update(repoId2, docId, clock2)
+
+    const clock3 = { abc123: 2, def456: 0 }
+    clockStore.update(repoId, docId, clock3)
+
+    const res1 = clockStore.get(repoId, docId)
+    const res2 = clockStore.get(repoId2, docId)
+    t.deepEqual(res1, clock3)
+    t.deepEqual(res2, clock2)
+    db.close()
+  })
 })
