fd45931b8a42782ca7176019ca951a7c08e64e7c
Jeff Peterson <jeff@yak.sh>
2019-10-15 15:15:57 -0700

    Export a few types and bump version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,6 +3,8 @@ export { Repo } from './Repo';
 export { Handle } from './Handle';
 export { RepoBackend } from './RepoBackend';
 export { RepoFrontend } from './RepoFrontend';
+export { ToFrontendRepoMsg, ToBackendRepoMsg } from './RepoMsg';
 export { DocBackend } from './DocBackend';
 export { DocFrontend } from './DocFrontend';
 export { DocUrl, HyperfileUrl } from './Misc';
+export { Header as HyperfileHeader } from './FileStore';
