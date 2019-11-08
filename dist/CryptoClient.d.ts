80b3eb100ecbf698016a59cfaeefc7178a464d5a
Matt Tognetti <mjtognetti@gmail.com>
2019-11-08 15:24:12 -0800

    yarn build and bump beta version

--- dist/CryptoClient.d.ts
+++ dist/CryptoClient.d.ts
@@ -5,9 +5,9 @@ export declare type RequestFn = (msg: ToBackendQueryMsg, cb: (msg: any) => void)
 export default class CryptoClient {
     request: RequestFn;
     constructor(request: RequestFn);
-    sign: (url: DocUrl, message: string) => Promise<Crypto.EncodedSignature>;
-    verify: (url: DocUrl, message: string, signature: Crypto.EncodedSignature) => Promise<boolean>;
-    sealedBox: (publicKey: Crypto.EncodedPublicEncryptionKey, message: string) => Promise<Crypto.EncodedSealedBox>;
-    openSealedBox: (keyPair: Crypto.EncodedEncryptionKeyPair, sealedBox: Crypto.EncodedSealedBox) => Promise<string>;
-    encryptionKeyPair: () => Promise<Crypto.EncodedEncryptionKeyPair>;
+    sign(url: DocUrl, message: string): Promise<Crypto.EncodedSignature>;
+    verify(url: DocUrl, message: string, signature: Crypto.EncodedSignature): Promise<boolean>;
+    sealedBox(publicKey: Crypto.EncodedPublicEncryptionKey, message: string): Promise<Crypto.EncodedSealedBox>;
+    openSealedBox(keyPair: Crypto.EncodedEncryptionKeyPair, sealedBox: Crypto.EncodedSealedBox): Promise<string>;
+    encryptionKeyPair(): Promise<Crypto.EncodedEncryptionKeyPair>;
 }
--- dist/CryptoClient.js
+++ dist/CryptoClient.js
@@ -3,52 +3,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Metadata_1 = require("./Metadata");
 class CryptoClient {
     constructor(request) {
-        this.sign = (url, message) => {
-            return new Promise((res, rej) => {
-                const docId = Metadata_1.validateDocURL(url);
-                this.request({ type: 'SignMsg', docId, message }, (msg) => {
-                    if (msg.success)
-                        return res(msg.signature);
-                    rej();
-                });
-            });
-        };
-        this.verify = (url, message, signature) => {
-            return new Promise((res) => {
-                const docId = Metadata_1.validateDocURL(url);
-                this.request({ type: 'VerifyMsg', docId, message, signature }, (msg) => {
-                    res(msg.success);
-                });
-            });
-        };
-        this.sealedBox = (publicKey, message) => {
-            return new Promise((res, rej) => {
-                this.request({ type: 'SealedBoxMsg', publicKey, message }, (msg) => {
-                    if (msg.success)
-                        return res(msg.sealedBox);
-                    rej();
-                });
-            });
-        };
-        this.openSealedBox = (keyPair, sealedBox) => {
-            return new Promise((res, rej) => {
-                this.request({ type: 'OpenSealedBoxMsg', keyPair, sealedBox }, (msg) => {
-                    if (msg.success)
-                        return res(msg.message);
-                    rej();
-                });
-            });
-        };
-        this.encryptionKeyPair = () => {
-            return new Promise((res, rej) => {
-                this.request({ type: 'EncryptionKeyPairMsg' }, (msg) => {
-                    if (msg.success)
-                        return res(msg.keyPair);
-                    rej();
-                });
-            });
-        };
         this.request = request;
+    }
+    sign(url, message) {
+        return new Promise((res, rej) => {
+            const docId = Metadata_1.validateDocURL(url);
+            this.request({ type: 'SignMsg', docId, message }, (msg) => {
+                if (msg.success)
+                    return res(msg.signature);
+                rej();
+            });
+        });
+    }
+    verify(url, message, signature) {
+        return new Promise((res) => {
+            const docId = Metadata_1.validateDocURL(url);
+            this.request({ type: 'VerifyMsg', docId, message, signature }, (msg) => {
+                res(msg.success);
+            });
+        });
+    }
+    sealedBox(publicKey, message) {
+        return new Promise((res, rej) => {
+            this.request({ type: 'SealedBoxMsg', publicKey, message }, (msg) => {
+                if (msg.success)
+                    return res(msg.sealedBox);
+                rej();
+            });
+        });
+    }
+    openSealedBox(keyPair, sealedBox) {
+        return new Promise((res, rej) => {
+            this.request({ type: 'OpenSealedBoxMsg', keyPair, sealedBox }, (msg) => {
+                if (msg.success)
+                    return res(msg.message);
+                rej();
+            });
+        });
+    }
+    encryptionKeyPair() {
+        return new Promise((res, rej) => {
+            this.request({ type: 'EncryptionKeyPairMsg' }, (msg) => {
+                if (msg.success)
+                    return res(msg.keyPair);
+                rej();
+            });
+        });
     }
 }
 exports.default = CryptoClient;
